129ed3af35742deda051c9d0b511df85cc232b6a
Harm Brouwer <me@hbrouwer.eu>
2013-01-21 15:56:55 +0100

    commented randomization functions, and fixed a bug in Fan-In randomization

---
+++ src/random.c
@@ -0,0 +1,155 @@
+/*
+ * random.c
+ *
+ * Copyright 2012, 2013 Harm Brouwer <me@hbrouwer.eu>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <math.h>
+
+#include "math.h"
+#include "random.h"
+
+/*
+ * Randomizes the values of a matrix using samples from a Gaussian normal
+ * distribution N(mu,sigma).
+ */
+
+void randomize_gaussian(struct matrix *m, struct network *n)
+{
+        for (int i = 0; i < m->rows; i++)
+                for (int j = 0; j < m->cols; j++)
+                        m->elements[i][j] = normrand(n->random_mu, n->random_sigma);
+}
+
+/* 
+ * Randomizes a matrix with values in a given range.
+ */
+
+void randomize_range(struct matrix *m, struct network *n)
+{
+        for (int i = 0; i < m->rows; i++)
+                for (int j = 0; j < m->cols; j++)
+                        m->elements[i][j] = (double)rand()
+                                / RAND_MAX
+                                * (n->random_max - n->random_min)
+                                + n->random_min;
+}
+
+/*
+ * Randomize a matrix using Nguyen-Widrow (NW; Nguyen & Widrow, 1990)
+ * randomization. In NW randomization, all weights are first randomized to
+ * values within a range [min,max]. We then compute the Euclidean norm of
+ * the weight matrix:
+ *
+ * en = sqrt(sum_i (w_ij ^ 2))
+ *
+ * as well as a beta value:
+ *
+ * beta = 0.7 * h ^ (1 / i)
+ *
+ * where h is the number of neurons in the group that is being projected to,
+ * and i the number of units in the projecting group. Based on this beta
+ * value and the Euclidean norm, each weight is then adjusted to:
+ *
+ * w_ij = (beta * w_ij) / en
+ *
+ * References
+ *
+ * Nguyen, D. & Widrow, B. (1990). Improving the learning speed of 2-layer
+ *     neural networks by choosing initial values of adaptive weights.
+ *     Proceedings of the International Joint Conference on Neural Networks
+ *     (IJCNN), 3:21-26, June 1990.
+ */
+
+void randomize_nguyen_widrow(struct matrix *m, struct network *n)
+{
+        randomize_range(m, n);
+        
+        /* 
+         * Compute Euclidean norm:
+         *
+         * en = sqrt(sum_i (w_ij ^ 2))
+         */
+        double en = 0.0;
+        for (int i = 0; i < m->rows; i++)
+                for (int j = 0; j < m->cols; j++)
+                        en += pow(m->elements[i][j], 2.0);
+        en = sqrt(en);
+
+        /* 
+         * Compute beta value:
+         * 
+         * beta = 0.7 * h ^ (1 / i)
+         */
+        double beta = 0.7 * pow(m->cols, 1.0 / m->rows);
+
+        /* compute weigths */
+        for (int i = 0; i < m->rows; i++)
+                for (int j = 0; j < m->cols; j++)
+                        m->elements[i][j] = (beta * m->elements[i][j]) / en;
+}
+
+
+/*
+ * Randomize a matrix using Fan-In (FI) randomization. In FI randomization,
+ * each weight is defined as:
+ *
+ * w_ij = (min / h) + R * ((max - min) / h)
+ *
+ * where h is the number of units in the group that is projected to and R
+ * is a random number in the [-1,1] range.
+ */
+
+void randomize_fan_in(struct matrix *m, struct network *n)
+{
+        /*
+         * Randomize weights in the [-1,1] range.
+         */
+        double random_min = n->random_min;
+        double random_max = n->random_max;
+
+        n->random_min = -1;
+        n->random_max = 1;
+
+        randomize_range(m, n);
+
+        n->random_min = random_min;
+        n->random_max = random_max;
+
+        /*
+         * Compute the weights:
+         *
+         * w_ij = (min / h) + w_ij * ((max - min) / h)
+         *
+         * where h is the number of units in the group
+         * that is projected to.
+         */
+        for (int i = 0; i < m->rows; i++)
+                for (int j = 0; j < m->cols; j++)
+                        m->elements[i][j] = n->random_min / m->cols
+                                + m->elements[i][j]
+                                * ((n->random_max - n->random_min) / m->cols);
+}
+
+/*
+ * Randomizes a matrix with binary values.
+ */
+
+void randomize_binary(struct matrix *m, struct network *n)
+{
+        for (int i = 0; i < m->rows; i++)
+                for (int j = 0; j < m->cols; j++)
+                        m->elements[i][j] = round((double)rand() / RAND_MAX);
+}
---
+++ src/random.h
@@ -0,0 +1,30 @@
+/*
+ * random.h
+ *
+ * Copyright 2012, 2013 Harm Brouwer <me@hbrouwer.eu>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include "network.h"
+
+void randomize_gaussian(struct matrix *m, struct network *n);
+void randomize_range(struct matrix *m, struct network *n);
+void randomize_nguyen_widrow(struct matrix *m, struct network *n);
+void randomize_fan_in(struct matrix *m, struct network *n);
+void randomize_binary(struct matrix *m, struct network *n);
+
+#endif /* RANDOM_H */
