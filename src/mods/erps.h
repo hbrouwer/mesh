f03e5a39958f03ae1f6b35ceb0b6d74d7fa3979c
Harm Brouwer <me@hbrouwer.eu>
2013-08-13 13:35:51 +0200

    added erps module

---
+++ src/mods/erps.h
@@ -0,0 +1,22 @@
+/*
+ * erps.h
+ *
+ * Copyright 2012, 2013 Harm Brouwer <me@hbrouwer.eu>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ERPS_H
+#define ERPS_H
+
+#endif /* ERPS_H */
